5802be6ec601f9df792142039b148fe71f83f29b
Andiie <andre.sousa.tec@gmail.com>
2021-09-05 18:24:36 -0300

    finalizando controller

--- src/main/java/br/com/estudoscrud/Todo/controllers/TodoController.java
+++ src/main/java/br/com/estudoscrud/Todo/controllers/TodoController.java
@@ -5,18 +5,18 @@ import br.com.estudoscrud.Todo.models.Todo;
 import br.com.estudoscrud.Todo.services.TodoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/v1/todo")
+@RestController //marca a classe como uma contoller para o spring gerenciar e receber requisições http
+@RequestMapping("/api/v1/todo") //define o caminho raiz para essa controler
 public class TodoController {
 
     TodoService todoService;
 
+    //o construtor resolve a necessidade de usar a notação @autowired do spring
+    //porque o spring entende que o serviço deve ser inicializado
     public TodoController(TodoService todoService){
         this.todoService = todoService;
     }
@@ -26,6 +26,19 @@ public class TodoController {
     public ResponseEntity<List<Todo>> getAllTodos(){
         List<Todo> todos = todoService.getTodos();
         return new ResponseEntity<>(todos, HttpStatus.OK);
+    }
+
+    //get devolve um item do banco com base no id na URL
+    @GetMapping({"/{todoId}"})
+    public ResponseEntity<Todo> getTodo(@PathVariable("todoId") Long todoId){
+        return new ResponseEntity<>(todoService.getTodoById(todoId),HttpStatus.OK);
+    }
+
+    //anotacao para tratar requisições PUT
+    @PutMapping({"/{todoId}"})
+    public ResponseEntity<Todo> updateTodo(@PathVariable("todoId") Long todoId){
+        todoService.deleteTodo(todoId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
